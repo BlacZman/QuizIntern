828073d931ce692804d0fa2f1f7dd8a03f24c149
BlacZman <37856140+BlacZman@users.noreply.github.com>
2021-02-24 23:39:38 +0700

    Add files via upload

--- 4/4th-1.py
+++ 4/4th-1.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from logging import log
 import urllib3, certifi, re
 
 url = "https://theinternship.io"
@@ -7,7 +8,14 @@ url = "https://theinternship.io"
 poolmanager = urllib3.PoolManager(ca_certs=certifi.where())
 res = poolmanager.request('GET', url)
 html = res.data.decode('utf-8')
-regtext = "<img src=\"(.*?)\" alt=\"(.*?)\" class=\"center-logos\" (.*?)>"
-logo = re.findall("<a href=\"(.*?)\" target=\"_blank\" (.*?)>" + regtext + "</a>", html)
-for item in logo:
-    print(item[2])
+img = "<img src=\"(.*?)\" alt=\"(.*?)\" class=\"center-logos\" data-v-018ba4ef>"
+logo = re.findall("<a href=\"(.*?)\" target=\"_blank\" data-v-018ba4ef>" + img + "</a>", html)
+about = re.findall("<span class=\"list-company\" data-v-018ba4ef>(.*?)</span>", html)
+
+newlogo = {}
+for i in range(len(logo)):
+    newlogo.update({logo[i][1]: about[i]})
+
+result = {key: val for key, val in sorted(newlogo.items(), key = lambda ele: len(ele[1]), reverse=False)}
+for item in result:
+    print(item)
--- 4/4th-2.py
+++ 4/4th-2.py
@@ -16,11 +16,19 @@ def getCompaniesPicture():
     poolmanager = urllib3.PoolManager(ca_certs=certifi.where())
     res = poolmanager.request('GET', url)
     html = res.data.decode('utf-8')
-    regtext = "<img src=\"(.*?)\" alt=\"(.*?)\" class=\"center-logos\" (.*?)>"
-    logoes = re.findall("<a href=\"(.*?)\" target=\"_blank\" (.*?)>" + regtext + "</a>", html)
+    img = "<img src=\"(.*?)\" alt=\"(.*?)\" class=\"center-logos\" data-v-018ba4ef>"
+    logo = re.findall("<a href=\"(.*?)\" target=\"_blank\" data-v-018ba4ef>" + img + "</a>", html)
+    about = re.findall("<span class=\"list-company\" data-v-018ba4ef>(.*?)</span>", html)
+
+    newlogo = {}
+    for i in range(len(logo)):
+        newlogo.update({logo[i][1]: about[i]})
+
+    shortestlogo = {key: val for key, val in sorted(newlogo.items(), key = lambda ele: len(ele[1]), reverse=False)}
+    
     result = {"companies": []}
-    for item in logoes:
-        logo = {"logo": url + item[2]}
+    for item in shortestlogo:
+        logo = {"logo": url + item}
         result["companies"].append(logo)
     resultjson = json.dumps(result, sort_keys=True, indent=4)
     response = make_response(jsonify(result), 200)
